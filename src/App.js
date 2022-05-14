35aef5f666c16b42ec6bab1e7f0a13fca56e8bd4
Abhinav <abhinav.maurya202@gmail.com>
2022-05-14 12:47:31 +0530

    started the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <div className="App"></div>
+    </Router>
   );
 }
 
